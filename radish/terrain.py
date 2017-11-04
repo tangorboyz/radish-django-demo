78b5d2632034882fcbe926bd2d2e1baeccad29a0
tangorboyz <vincentiusdhawu@gmail.com>
2017-11-04 11:43:48 +0800

    django setup before scenario, test runner on each of them

--- radish/terrain.py
+++ radish/terrain.py
@@ -1,6 +1,7 @@
 import os
 import django
 import warnings
+import datetime
 from radish import before, after, world
 from django.test import TestCase
 from django.test.runner import DiscoverRunner
@@ -12,33 +13,26 @@ BASE_URL = os.environ.get('BASE_URL', 'http://localhost:8000')
 
 
 @before.all
-def setup_django(features, marker):
-    warnings.filterwarnings('ignore', category=DeprecationWarning)
-    django.setup()
-    world.test_runner = DiscoverRunner(interactive=False, verbosity=0)
-    world.test_runner.setup_test_environment()
-    world.old_db_config = world.test_runner.setup_databases()
+def start_timer(features, marker):
+    world.start_time = datetime.datetime.now()
 
 
 @after.all
-def tear_down_django(features, marker):
-    world.test_runner.teardown_databases(world.old_db_config)
-    world.test_runner.teardown_test_environment()
-
-
-@before.each_feature
-def set_up_feature(feature):
-    world.test_class.setUpClass()
-
-
-@after.each_feature
-def tear_down_feature(feature):
-    world.test_class.tearDownClass()
+def stop_timer(features, marker):
+    elapsed = world.start_time - datetime.datetime.now()
+    print("custom timer: " + str(elapsed))
 
 
 @before.each_scenario
 def set_up_scenario(scenario):
-    scenario.context.test_case = world.test_class()
+    warnings.filterwarnings('ignore', category=DeprecationWarning)
+    django.setup()
+    scenario.context.test_runner = DiscoverRunner(interactive=False, verbosity=0)
+    scenario.context.test_runner.setup_test_environment()
+    scenario.context.old_db_config = scenario.context.test_runner.setup_databases()
+    scenario.context.test_class = TestCase
+    scenario.context.test_class.setUpClass()
+    scenario.context.test_case = scenario.context.test_class()
     scenario.context.test_case._pre_setup()
     scenario.context.browser = webdriver.Chrome()
     scenario.context.base_url = BASE_URL
@@ -48,3 +42,6 @@ def set_up_scenario(scenario):
 def tear_down_scenario(scenario):
     scenario.context.browser.quit()
     scenario.context.test_case._post_teardown()
+    scenario.context.test_class.tearDownClass()
+    scenario.context.test_runner.teardown_databases(scenario.context.old_db_config)
+    scenario.context.test_runner.teardown_test_environment()
